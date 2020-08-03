86703fdd23bfd3620003d1ca56460aed6ac15945
Vodkhard <hugo.knorr@gmail.com>
2020-08-03 17:05:59 +0200

    limit number of parsing services

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,8 +11,21 @@ final class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('orchestra');
-        $treeBuilder->getRootNode()->end();
+        if (method_exists(TreeBuilder::class, 'getRootNode')) {
+            $treeBuilder = new TreeBuilder('api_platform');
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $treeBuilder = new TreeBuilder();
+            $rootNode = $treeBuilder->root('api_platform');
+        }
+
+        $rootNode
+            ->children()
+                ->arrayNode('resource_class_directories')
+                    ->prototype('scalar')->end()
+                ->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
